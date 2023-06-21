f0a4bbc4dda872635ca5ece13d9acb07d4608a31
Geocodlea <geocodlea@yahoo.com>
2023-06-22 00:33:57 +0300

    test deploy

--- api/index.js
+++ api/index.js
@@ -1,147 +1,149 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
-// /**
-//  * Module dependencies.
-//  */
+/**
+ * Module dependencies.
+ */
 
-// const createError = require("http-errors");
-// const express = require("express");
-// const path = require("path");
-// const cookieParser = require("cookie-parser");
-// const logger = require("morgan");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-// const indexRouter = require("../src/routes/index");
-// const categoryRouter = require("../src/routes/category");
-// const placeRouter = require("../src/routes/place");
+const app = express();
 
-// const app = express();
+var debug = require("debug")("project-1:server");
+var http = require("http");
 
-// var debug = require("debug")("project-1:server");
-// var http = require("http");
+/**
+ * Get port from environment and store in Express.
+ */
 
-// /**
-//  * Get port from environment and store in Express.
-//  */
+var port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-// var port = normalizePort(process.env.PORT || "3000");
-// app.set("port", port);
+/**
+ * Create HTTP server.
+ */
 
-// /**
-//  * Create HTTP server.
-//  */
+var server = http.createServer(app);
 
-// var server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-// /**
-//  * Listen on provided port, on all network interfaces.
-//  */
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
 
-// server.listen(port);
-// server.on("error", onError);
-// server.on("listening", onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-// /**
-//  * Normalize a port into a number, string, or false.
-//  */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
 
-// function normalizePort(val) {
-//   var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-//   if (isNaN(port)) {
-//     // named pipe
-//     return val;
-//   }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-//   if (port >= 0) {
-//     // port number
-//     return port;
-//   }
+  return false;
+}
 
-//   return false;
-// }
+/**
+ * Event listener for HTTP server "error" event.
+ */
 
-// /**
-//  * Event listener for HTTP server "error" event.
-//  */
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-// function onError(error) {
-//   if (error.syscall !== "listen") {
-//     throw error;
-//   }
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-//   var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
 
-//   // handle specific listen errors with friendly messages
-//   switch (error.code) {
-//     case "EACCES":
-//       console.error(bind + " requires elevated privileges");
-//       process.exit(1);
-//       break;
-//     case "EADDRINUSE":
-//       console.error(bind + " is already in use");
-//       process.exit(1);
-//       break;
-//     default:
-//       throw error;
-//   }
-// }
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
-// /**
-//  * Event listener for HTTP server "listening" event.
-//  */
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
 
-// function onListening() {
-//   var addr = server.address();
-//   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-//   debug("Listening on " + bind);
-// }
+app.get("/api", (req, res) => {
+  res.send("EUREKA on Vercel");
+});
 
-// mongoose.connect(`${process.env.MONGODB_URI}`);
+const indexRouter = require("../src/routes/index");
+const categoryRouter = require("../src/routes/category");
+const placeRouter = require("../src/routes/place");
 
-// // default mockup
-// require("../src/mockup");
+mongoose.connect(`${process.env.MONGODB_URI}`);
 
-// // view engine setup
-// app.set("views", path.join(__dirname, "../src/views"));
-// app.set("view engine", "ejs");
+// default mockup
+require("../src/mockup");
 
-// app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(cors());
+// view engine setup
+app.set("views", path.join(__dirname, "../src/views"));
+app.set("view engine", "ejs");
 
-// app.use("/", indexRouter);
-// app.use("/category", categoryRouter);
-// app.use("/category/place", placeRouter);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+app.use("/", indexRouter);
+app.use("/category", categoryRouter);
+app.use("/category/place", placeRouter);
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-// module.exports = app;
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
 
 const express = require("express");
 const app = express();
-app.get("/api", (req, res) => {
-  res.send("Express on Vercel");
-});
+
 app.listen(5001, () => {
   console.log("Running on port 5001.");
 });
