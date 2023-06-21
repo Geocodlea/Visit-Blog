b30009f3a3be51bb442ee34ce76c61918d46fec4
Geocodlea <geocodlea@yahoo.com>
2023-06-21 23:51:11 +0300

    test deploy

--- api/index.js
+++ api/index.js
@@ -1,18 +1,91 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+// #!/usr/bin/env node
 
-const indexRouter = require("./routes/index");
-const categoryRouter = require("./routes/category");
-const placeRouter = require("./routes/place");
+// /**
+//  * Module dependencies.
+//  */
 
-const app = express();
+// var app = require("../src/index");
+// var debug = require("debug")("project-1:server");
+// var http = require("http");
 
+// /**
+//  * Get port from environment and store in Express.
+//  */
+
+// var port = normalizePort(process.env.PORT || "3000");
+// app.set("port", port);
+
+// /**
+//  * Create HTTP server.
+//  */
+
+// var server = http.createServer(app);
+
+// /**
+//  * Listen on provided port, on all network interfaces.
+//  */
+
+// server.listen(port);
+// server.on("error", onError);
+// server.on("listening", onListening);
+
+// /**
+//  * Normalize a port into a number, string, or false.
+//  */
+
+// function normalizePort(val) {
+//   var port = parseInt(val, 10);
+
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
+
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
+
+//   return false;
+// }
+
+// /**
+//  * Event listener for HTTP server "error" event.
+//  */
+
+// function onError(error) {
+//   if (error.syscall !== "listen") {
+//     throw error;
+//   }
+
+//   var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+//   // handle specific listen errors with friendly messages
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// }
+
+// /**
+//  * Event listener for HTTP server "listening" event.
+//  */
+
+// function onListening() {
+//   var addr = server.address();
+//   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+//   debug("Listening on " + bind);
+// }
+
+const app = require("../src/index");
 const debug = require("debug")("project-1:server");
 const http = require("http");
 
@@ -66,40 +139,5 @@ function onListening() {
   debug("Listening on " + bind);
 }
 
-mongoose.connect(`${process.env.MONGODB_URI}`);
-
-// default mockup
-require("./mockup");
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
-
-app.use("/", indexRouter);
-app.use("/category", categoryRouter);
-app.use("/category/place", placeRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
+// Export the Express API
 module.exports = app;
